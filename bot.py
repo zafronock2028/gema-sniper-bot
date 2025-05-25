34201978c5b81e00a2fb0097e0751f496b8ab32c
zafronock2028 <zafronockbitfratsg@gmail.com>
2025-05-25 00:33:35 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,46 @@
+
+import asyncio
+import json
+import websockets
+import requests
+import os
+
+TELEGRAM_TOKEN = "8124603541:AAHZzrF_-0T85KjjkMwHq0O-PhWIxT2A6fI"
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+async def send_telegram_message(message: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+    try:
+        requests.post(url, json=payload)
+    except Exception as e:
+        print("Error sending message:", e)
+
+async def listen_new_tokens():
+    uri = "wss://pumpportal.fun/api/data"
+    async with websockets.connect(uri) as websocket:
+        # Suscribirse a eventos de nuevos tokens
+        await websocket.send(json.dumps({ "method": "subscribeNewToken" }))
+
+        while True:
+            try:
+                response = await websocket.recv()
+                data = json.loads(response)
+
+                if data.get("type") == "new_token":
+                    token = data.get("data", {})
+                    name = token.get("name", "N/A")
+                    address = token.get("address", "N/A")
+                    msg = f"<b>Nuevo token detectado:</b> {name}\n"
+                    msg += f"<a href='https://pump.fun/{address}'>Ir al token</a>"
+                    await send_telegram_message(msg)
+
+            except Exception as e:
+                print("Error:", e)
+
+if __name__ == "__main__":
+    asyncio.run(listen_new_tokens())
